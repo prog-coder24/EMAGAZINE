01f4ccd7834f287534204a3a7f04cd4f690a5c27
imaaditya-stack <vaishnavisawant1224@gmail.com>
2020-10-24 20:34:37 +0530

    modified menu.html

--- EMAG_APP/urls.py
+++ EMAG_APP/urls.py
@@ -5,10 +5,10 @@ from .views import *
 
 urlpatterns = [
     path('', home_view),
-    path('add_event/', event_view),
+    path('events/', event_view),
     path('menu/', menu_view),
-    path('achievement/', achievement_view),
-    path('project/', project_view),
+    path('achievements/', achievement_view),
+    path('projects/', project_view),
     path('welcome/', home_view),
 
 ]
