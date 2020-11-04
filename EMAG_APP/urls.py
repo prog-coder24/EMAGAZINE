f42443375b103f8630fa0aeb14fd4bff80cee5d2
imaaditya-stack <vaishnavisawant1224@gmail.com>
2020-11-04 12:40:15 +0530

    created display page

--- EMAG_APP/urls.py
+++ EMAG_APP/urls.py
@@ -6,9 +6,18 @@ from .views import *
 urlpatterns = [
     path('', home_view),
     path('menu/events/', event_view),
+    path('events/<int:pk>', displayevent_view),
+    path('events/', displayevent_view),
     path('menu/', menu_view),
-    path('achievements/', achievement_view),
-    path('projects/', project_view),
+    path('menu/achievements/', achievement_view),
+    path('achievements/<int:pk>', displayach_view),
+    path('achievements/', displayach_view),
+    path('menu/projects/', project_view),
+    path('projects/<int:pk>', displayproj_view),
+    path('projects/', displayproj_view),
+    path('login/', login_view),
+    path('logout/', logout_view),
+    path('success/', authenticate_view),
     path('welcome/', home_view),
 
 ]
--- EMAG_APP/views.py
+++ EMAG_APP/views.py
@@ -11,18 +11,87 @@ from .models import *
 def home_view(request):
     return render(request, "home.html")
 
+
 def menu_view(request):
     return render(request, "menu/menu.html")
+
 
 def event_view(request):
 
     events = Event.objects.all()
-    return render(request, "event/event.html", {"events" : events})
+    return render(request, "event/event.html", {"events": events})
+
+
+def displayevent_view(request, pk):
+
+    try:
+        event = Event.objects.get(pk=pk)
+        return render(request, "event/displayevent.html", {"event": event})
+
+    except Event.DoesNotExist:
+        return render(request, "home.html")
+
+    return None
+
 
 def project_view(request):
-    return render(request, "project/project.html")
+
+    projects = Project.objects.all()
+    return render(request, "project/project.html", {"projects": projects})
+
+
+def displayproj_view(request, pk):
+
+    try:
+        project = Project.objects.get(pk=pk)
+        return render(request, "project/displayproj.html", {"project": project})
+
+    except Project.DoesNotExist:
+        return render(request, "home.html")
+
+    return None
+
 
 def achievement_view(request):
-    return render(request, "achievement/achievement.html")
+
+    achievements = Achievement.objects.all()
+    return render(request, "achievement/achievement.html", {"achievements": achievements})
 
 
+def displayach_view(request, pk):
+
+    try:
+        achievement = Achievement.objects.get(pk=pk)
+        return render(request, "achievement/displayach.html", {"achievement": achievement})
+
+    except Achievement.DoesNotExist:
+        return render(request, "home.html")
+
+    return None
+
+
+def login_view(request):
+    return render(request, "emag_admin/login.html")
+
+
+def logout_view(request):
+
+    logout(request)
+    return redirect(login_view)
+
+
+def authenticate_view(request):
+
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        user = authenticate(username=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect(event_form_view)
+        else:
+            return render(request, "emag_admin/login.html", {"error": "Invalid Credentials"})
+
+    return render(request, "emag_admin/login.html")
